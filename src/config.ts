4d576cb4a1a522ef42f7d7d9c77aa6c341269f6f
Saulius <sauliuspetr@gmail.com>
2025-07-29 15:53:13 +0300

    Create centralized site configuration

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const SITE = {
+  title: "#TagChoose - The Smart Bookmark Manager",
+  description: "The smart way to organize your digital life. Save links, text, and images with flexible tags, now enhanced with AI-powered suggestions. Ditch your folders and find your focus.",
+  gwsLink: "https://chromewebstore.google.com/detail/tagchoose-bookmark-manage/hlfgdfpeekcelanebbfchnnneijhophh"
+};
