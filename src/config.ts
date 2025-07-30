60111fabfa07626da9cddd7c4afd2b4451cb701e
Saulius <sauliuspetr@gmail.com>
2025-07-30 16:55:49 +0300

    Implement WhoBenefits component and integrate it

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,15 @@
 export const SITE = {
   title: "#TagChoose - The Smart Bookmark Manager",
   description: "The smart way to organize your digital life. Save links, text, and images with flexible tags, now enhanced with AI-powered suggestions. Ditch your folders and find your focus.",
-  gwsLink: "https://chromewebstore.google.com/detail/tagchoose-bookmark-manage/hlfgdfpeekcelanebbfchnnneijhophh"
+  gwsLink: "https://chromewebstore.google.com/detail/tagchoose-bookmark-manage/hlfgdfpeekcelanebbfchnnneijhophh",
+  userProfiles: [
+    'Researchers',
+    'Developers',
+    'Students',
+    'Product Managers',
+    'Writers',
+    'Designers',
+    'Content Creators',
+    'Productivity Enthusiasts',
+  ],
 };
