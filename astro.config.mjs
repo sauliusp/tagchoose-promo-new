32cc4c372ab1c7bdf940deca0ca8f72c86ab5d2c
Saulius <sauliuspetr@gmail.com>
2025-08-01 12:41:34 +0300

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  // Add the 'site' property here
+  site: 'https://tagchoose.site', 
+  
   integrations: [tailwind()],
 });
