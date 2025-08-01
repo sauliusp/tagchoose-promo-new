9f479ef354903e9581bb6135befa01a539300f55
Saulius <sauliuspetr@gmail.com>
2025-08-01 10:23:47 +0300

    Updated config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@ export const SITE = {
   description: "The smart way to organize your digital life. Save webpages with AI-powered suggestions. Ditch your folders and find your focus.",
   gwsLink: "https://chromewebstore.google.com/detail/tagchoose-bookmark-manage/hlfgdfpeekcelanebbfchnnneijhophh",
   email: 'saulius.developer@gmail.com',
+  name: 'Saulius'
 };
