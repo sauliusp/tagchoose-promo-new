e299b3003683640fa17f2339411d357c2567a8ba
Saulius <sauliuspetr@gmail.com>
2025-08-01 10:18:57 +0300

    Updated config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE = {
   title: "#TagChoose - The Smart Bookmark Manager",
-  description: "The smart way to organize your digital life. Save links, text, and images with flexible tags, now enhanced with AI-powered suggestions. Ditch your folders and find your focus.",
+  description: "The smart way to organize your digital life. Save webpages with AI-powered suggestions. Ditch your folders and find your focus.",
   gwsLink: "https://chromewebstore.google.com/detail/tagchoose-bookmark-manage/hlfgdfpeekcelanebbfchnnneijhophh",
   email: 'saulius.developer@gmail.com',
   userProfiles: [
